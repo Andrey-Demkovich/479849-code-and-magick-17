ff974eb7c324c6fdbff91d9a5bc15ee7c3341534
Andrey-Demkovich <a.demkovich@tut.by>
2019-06-12 17:00:04 +0300

    Исправил замечания наставника

--- js/setup.js
+++ js/setup.js
@@ -35,12 +35,19 @@ var WIZARDS_PROPERTIES = {
   EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
 };
 
+var userDialog = document.querySelector('.setup');
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+var similarWizardTemplate = document
+  .querySelector('#similar-wizard-template')
+  .content.querySelector('.setup-similar-item');
+
 var generateRandomInteger = function (min, max) {
   return min + Math.floor((max + 1 - min) * Math.random());
 };
 
 var generateWizards = function (properties) {
   var cloneNames = properties.NAMES.slice();
+  var cloneSurnames = properties.SURNAMES.slice();
   var wizards = [];
 
   for (var i = 0; i < properties.SURNAMES.length; i++) {
@@ -49,7 +56,7 @@ var generateWizards = function (properties) {
     wizards[i].name =
       cloneNames.splice(generateRandomInteger(0, cloneNames.length - 1), 1) +
       ' ' +
-      properties.SURNAMES[i];
+      cloneSurnames.splice(generateRandomInteger(0, cloneNames.length - 1), 1);
 
     wizards[i].coatColor =
       properties.COATS_COLORS[
@@ -65,18 +72,6 @@ var generateWizards = function (properties) {
   return wizards;
 };
 
-var wizards = generateWizards(WIZARDS_PROPERTIES);
-
-var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-userDialog.querySelector('.setup-similar').classList.remove('hidden');
-
-var similarListElement = userDialog.querySelector('.setup-similar-list');
-
-var similarWizardTemplate = document
-  .querySelector('#similar-wizard-template')
-  .content.querySelector('.setup-similar-item');
-
 var createWizard = function (wizard) {
   var wizardElement = similarWizardTemplate.cloneNode(true);
 
@@ -86,6 +81,11 @@ var createWizard = function (wizard) {
 
   return wizardElement;
 };
+
+userDialog.classList.remove('hidden');
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var wizards = generateWizards(WIZARDS_PROPERTIES);
 
 var fragment = document.createDocumentFragment();
 for (var i = 0; i < 4; i++) {
